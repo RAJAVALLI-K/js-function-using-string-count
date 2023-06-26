dac41a32029a770429b14fed7f9f5b243c0e3cc3
RAJAVALLI-K <rajavalli1932bca@gmail.com>
2023-06-26 15:48:17 +0530

    add js-function-using-string-count

---
+++ main.js
@@ -0,0 +1,10 @@
+// string count 
+
+function stringCount(names){
+    for(let i=0;i<names.length;i++){
+        var value=i;
+        console.log(value);
+    }
+    console.log(value);
+}
+stringCount("rajavalli");
